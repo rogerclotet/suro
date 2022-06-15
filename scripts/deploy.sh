df7c3459862e9a8f60b98b5509f6f71bd6765ed8
Roger Clotet <roger@clotet.dev>
2022-06-15 11:44:02 +0200

    CI and deployment setup

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+(
+  sshpass -p $SSH_PASSWORD ssh $SSH_USERNAME@$SSH_IP -o StrictHostKeyChecking=no <<-EOF
+    cd $SSH_PROJECT_DIRECTORY
+    git pull
+    cd $SSH_PROJECT_DIRECTORY/client
+    yarn install
+    yarn build
+    cd $SSH_PROJECT_DIRECTORY/server
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+    docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
+    DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
+      docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+EOF
+)
