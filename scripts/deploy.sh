f924d2e719d50510fff66a2ff9f1410bcc71c1f6
Roger Clotet <roger@clotet.dev>
2022-06-15 14:05:04 +0200

    Add docker-compose build step for production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,12 @@
     REACT_APP_API_URL=$API_URL yarn build
     cd $SSH_PROJECT_DIRECTORY/server
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+    DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
+        SECRET_KEY=$SECRET_KEY \
+        docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
     DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
-    SECRET_KEY=$SECRET_KEY \
-      docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+        SECRET_KEY=$SECRET_KEY \
+        docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 EOF
 )
