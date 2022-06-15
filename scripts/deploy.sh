0c2241715e8370296a919a90a8d9b23a0973c8f9
Roger Clotet <roger@clotet.dev>
2022-06-15 18:07:48 +0200

    Change static root in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@
     cd $SSH_PROJECT_DIRECTORY/server
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
     DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
-        SECRET_KEY=$SECRET_KEY \
+        SECRET_KEY=$SECRET_KEY STATIC_ROOT=$STATIC_ROOT \
         docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
     DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
-        SECRET_KEY=$SECRET_KEY \
+        SECRET_KEY=$SECRET_KEY STATIC_ROOT=$STATIC_ROOT \
         docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
     STATIC_ROOT=$STATIC_ROOT docker-compose -f docker-compose.yml -f docker-compose.prod.yml exec api python manage.py collectstatic
 EOF
