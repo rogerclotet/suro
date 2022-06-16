5985b691488368d6e799752415d2a06b3c07bacf
Roger Clotet <roger@clotet.dev>
2022-06-16 13:45:14 +0200

    Add tabs to lists

--- client/src/lists/Lists.jsx
+++ client/src/lists/Lists.jsx
@@ -1,16 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { Container, Grid } from '@mui/material'
+import { Box, Grid, Tab } from '@mui/material'
 import LoadingScreen from '../LoadingScreen'
 import { useHeader } from '../HeaderProvider'
 import ListPreview from './ListPreview'
 import NewListButton from './NewListButton'
 import useClient from '../useClient'
 import { Helmet } from 'react-helmet-async'
+import { TabContext, TabList, TabPanel } from '@mui/lab'
 
 const Lists = () => {
   const [lists, setLists] = useState()
   const { setHeader } = useHeader()
   const { listsRequest } = useClient()
+  const [tab, setTab] = useState('lists')
 
   const refreshLists = useCallback(() => {
     listsRequest()
@@ -32,22 +34,37 @@ const Lists = () => {
     return <LoadingScreen />
   }
 
+  const handleTabChange = (event, value) => {
+    setTab(value)
+  }
+
   return (
     <>
       <Helmet>
         <title>Llistes</title>
         <meta name="description" content="Gestor familiar de llistes" />
       </Helmet>
-      <Container sx={{ pt: 2, pb: 11 }}>
-        <Grid container direction="column" spacing={2}>
-          {lists.map(list => (
-            <Grid item key={list.id}>
-              <ListPreview list={list} onChange={refreshLists} />
-            </Grid>
-          ))}
-        </Grid>
-        <NewListButton onClose={refreshLists} />
-      </Container>
+
+      <TabContext value={tab}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleTabChange} variant="fullWidth">
+            <Tab label="Llistes" value="lists" />
+            <Tab label="Plantilles" value="templates" />
+          </TabList>
+        </Box>
+        <TabPanel value="lists">
+          <Grid container direction="column" spacing={2} sx={{ pb: 8 }}>
+            {lists.map(list => (
+              <Grid item key={list.id}>
+                <ListPreview list={list} onChange={refreshLists} />
+              </Grid>
+            ))}
+          </Grid>
+        </TabPanel>
+        <TabPanel value="templates">Plantilles</TabPanel>
+      </TabContext>
+
+      <NewListButton onClose={refreshLists} />
     </>
   )
 }
