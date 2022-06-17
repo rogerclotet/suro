7db50c4679bc6dc3dd8ee82b481d5eae26250349
Roger Clotet <roger@clotet.dev>
2022-06-17 13:27:16 +0200

    Fix cancel delete

--- client/src/lists/ListPreview.jsx
+++ client/src/lists/ListPreview.jsx
@@ -60,16 +60,14 @@ const ListPreview = ({ list, onChange }) => {
 
   const handleDeleteDialogClose = confirmed => {
     if (confirmed) {
-      listRequest(list.id, { method: 'DELETE' })
-        .then(res => {
-          setIsDeleting(false)
-          if (res.status === 204) {
-            onChange()
-          } else {
-            console.log('Error deleting list', res.status)
-          }
-        })
-        .catch(e => console.log('Error deleting list', e))
+      listRequest(list.id, { method: 'DELETE' }).then(res => {
+        setIsDeleting(false)
+        if (res.status === 204) {
+          onChange()
+        } else {
+          console.log('Error deleting list', res.status)
+        }
+      })
     } else {
       setIsDeleting(false)
     }
@@ -85,61 +83,62 @@ const ListPreview = ({ list, onChange }) => {
         onChange()
       } else {
         console.log('Error toggling favorite')
-        // TODO error feedback
       }
     })
   }
 
   return (
-    <Card>
-      <Link
-        component={RouterLink}
-        to={`/lists/${list.id}`}
-        underline="none"
-        color="inherit"
-      >
-        <CardContent>
-          <Typography variant="h7" component="h3">
-            {list.name}
-          </Typography>
-          {list.description && (
-            <Typography variant="subtitle1" color="text.secondary">
-              {list.description}
+    <>
+      <Card>
+        <Link
+          component={RouterLink}
+          to={`/lists/${list.id}`}
+          underline="none"
+          color="inherit"
+        >
+          <CardContent>
+            <Typography variant="h7" component="h3">
+              {list.name}
             </Typography>
-          )}
-          <Typography variant="body2">{`${list.items.length} elements`}</Typography>
-        </CardContent>
-      </Link>
-      <CardActions sx={{ justifyContent: 'flex-end' }}>
-        <IconButton
-          onClick={handleToggleFavorite}
-          color={list.is_favorite ? 'secondary' : 'inherit'}
-        >
-          <Favorite />
-        </IconButton>
-        <RWebShare
-          data={{
-            title: list.name,
-            text: `${list.name}%0D%0A%0D%0A${
-              list.description ? list.description + '%0D%0A' : ''
-            }Llista amb ${list.items.length} elements.`,
-            url: `${window.location.href}/${list.id}`,
-          }}
-        >
-          <IconButton>
-            <Share />
+            {list.description && (
+              <Typography variant="subtitle1" color="text.secondary">
+                {list.description}
+              </Typography>
+            )}
+            <Typography variant="body2">{`${list.items.length} elements`}</Typography>
+          </CardContent>
+        </Link>
+        <CardActions sx={{ justifyContent: 'flex-end' }}>
+          <IconButton
+            onClick={handleToggleFavorite}
+            color={list.is_favorite ? 'secondary' : 'inherit'}
+          >
+            <Favorite />
           </IconButton>
-        </RWebShare>
-        <IconButton onClick={() => setIsDeleting(true)}>
-          <DeleteConfirmationDialog
-            list={list}
-            open={isDeleting}
-            onClose={handleDeleteDialogClose}
-          />
-          <DeleteForever />
-        </IconButton>
-      </CardActions>
-    </Card>
+          <RWebShare
+            data={{
+              title: list.name,
+              text: `${list.name}%0D%0A%0D%0A${
+                list.description ? list.description + '%0D%0A' : ''
+              }Llista amb ${list.items.length} elements.`,
+              url: `${window.location.href}/${list.id}`,
+            }}
+          >
+            <IconButton>
+              <Share />
+            </IconButton>
+          </RWebShare>
+          <IconButton onClick={() => setIsDeleting(true)}>
+            <DeleteForever />
+          </IconButton>
+        </CardActions>
+      </Card>
+      <DeleteConfirmationDialog
+        list={list}
+        open={isDeleting}
+        onClose={handleDeleteDialogClose}
+      />
+    </>
   )
 }
 
