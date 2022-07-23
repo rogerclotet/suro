ec9e599435310227c8b08f1092654365145e6ee9
Roger Clotet <roger@clotet.dev>
2022-07-23 14:04:24 +0200

    Set project name for docker-compose containers

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,15 +10,15 @@
     REACT_APP_API_URL=$API_URL yarn build
 
     cd $SSH_PROJECT_DIRECTORY/server
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+    docker-compose -p familia -f docker-compose.yml -f docker-compose.prod.yml pull
     DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
         SECRET_KEY=$SECRET_KEY STATIC_ROOT=$STATIC_ROOT \
         docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
-    docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
+    docker-compose -p familia -f docker-compose.yml -f docker-compose.prod.yml stop
     DB_NAME=$DB_NAME DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD \
         SECRET_KEY=$SECRET_KEY STATIC_ROOT=$STATIC_ROOT \
-        docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-    STATIC_ROOT=$STATIC_ROOT docker-compose -f docker-compose.yml -f docker-compose.prod.yml \
+        docker-compose -p familia -f docker-compose.yml -f docker-compose.prod.yml up -d
+    STATIC_ROOT=$STATIC_ROOT docker-compose -p familia -f docker-compose.yml -f docker-compose.prod.yml \
         exec -T api \python manage.py collectstatic --no-input
 EOF
 )
