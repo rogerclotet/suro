01cec3ff71462afbe5b50d493ec2746a1a63cb9d
Roger Clotet <roger@clotet.dev>
2023-12-07 09:45:26 +0100

    Remove dynamic color

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:familia/auth/auth.dart';
 import 'package:familia/auth/login_screen.dart';
 import 'package:familia/client.dart';
@@ -6,6 +5,7 @@ import 'package:familia/families/global_state.dart';
 import 'package:familia/lists/lists_screen.dart';
 import 'package:familia/lists/lists_state.dart';
 import 'package:familia/routes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
@@ -14,8 +14,6 @@ import 'package:provider/provider.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'families/families_state.dart';
-
-const _defaultColor = Color.fromARGB(255, 61, 195, 126);
 
 void main() {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
@@ -44,7 +42,7 @@ class FamilyApp extends StatefulWidget {
 const primaryColor = Colors.teal;
 final defaultColorScheme = ColorScheme.fromSwatch(
   primarySwatch: primaryColor,
-  brightness: WidgetsBinding.instance.window.platformBrightness,
+  brightness: PlatformDispatcher.instance.platformBrightness,
 );
 
 class _FamilyAppState extends State<FamilyApp> {
@@ -111,43 +109,21 @@ class _FamilyAppState extends State<FamilyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => auth),
-        ListenableProvider(create: (_) => authClient),
-        ChangeNotifierProvider(create: (_) => familiesState),
-        ChangeNotifierProvider(create: (_) => listsState),
-      ],
-      child: DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-          final lightColorScheme = lightDynamic ??
-              ColorScheme.fromSeed(
-                seedColor: _defaultColor,
-              );
-          final darkColorScheme = darkDynamic ??
-              ColorScheme.fromSeed(
-                seedColor: _defaultColor,
-                brightness: Brightness.dark,
-              );
-
-          return Consumer<Auth>(
-            builder: (context, auth, child) {
-              return MaterialApp.router(
-                routeInformationParser: router.routeInformationParser,
-                routeInformationProvider: router.routeInformationProvider,
-                routerDelegate: router.routerDelegate,
-                title: 'Família',
-                themeMode: ThemeMode.system,
-                theme: ThemeData(
-                  colorScheme: lightColorScheme,
-                ),
-                darkTheme: ThemeData(
-                  colorScheme: darkColorScheme,
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (_) => auth),
+          ListenableProvider(create: (_) => authClient),
+          ChangeNotifierProvider(create: (_) => familiesState),
+          ChangeNotifierProvider(create: (_) => listsState),
+        ],
+        child: MaterialApp.router(
+          routeInformationParser: router.routeInformationParser,
+          routeInformationProvider: router.routeInformationProvider,
+          routerDelegate: router.routerDelegate,
+          title: 'Família',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+            brightness: PlatformDispatcher.instance.platformBrightness,
+          ),
+        ));
   }
 }
