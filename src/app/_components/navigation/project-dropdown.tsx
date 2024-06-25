d56edb084526e9f072a7470cf6d81d269544bf79
Roger Clotet <roger@clotet.dev>
2024-06-25 21:28:55 +0200

    Query user projects

--- src/app/_components/navigation/project-dropdown.tsx
+++ src/app/_components/navigation/project-dropdown.tsx
@@ -1,13 +1,25 @@
+import { auth } from "@/auth";
 import { db } from "@/server/db";
+import { projectToUsers } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
 
 export default async function ProjectSelector() {
-  const projects = await db.query.projects.findMany();
+  const session = await auth();
+
+  if (!session) {
+    return null;
+  }
+
+  const results = await db.query.projectToUsers.findMany({
+    with: { user: true, project: true },
+    where: eq(projectToUsers.userId, session.user.id),
+  });
 
   return (
     <select className="select select-bordered w-full max-w-xs">
-      {projects.map((project) => (
-        <option key={project.id} value={project.id}>
-          {project.name}
+      {results.map((result) => (
+        <option key={result.project.id} value={result.project.id}>
+          {result.project.name}
         </option>
       ))}
       <option value="new">+ Nou projecte</option>
