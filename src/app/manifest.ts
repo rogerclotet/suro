ba9b898d961bdd93885d926f995ae8ce6c0d31b1
Roger Clotet <roger@clotet.dev>
2024-06-26 22:08:35 +0200

    Change manifest to dynamic

---
+++ src/app/manifest.ts
@@ -0,0 +1,21 @@
+import { type MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Família",
+    short_name: "Família",
+    description: "Gestor familiar",
+    start_url: "/",
+    display: "standalone",
+    orientation: "portrait",
+    background_color: "#2A303C",
+    theme_color: "#9FE88D",
+    icons: [
+      {
+        src: "/favicon.png",
+        sizes: "any",
+        type: "image/png",
+      },
+    ],
+  };
+}
