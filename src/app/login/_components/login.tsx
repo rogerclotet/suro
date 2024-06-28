180c628997650af0b77ba68c5b97bb5d6c74c91c
Roger Clotet <roger@clotet.dev>
2024-06-28 20:23:30 +0200

    Fix login layout

---
+++ src/app/login/_components/login.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import type { Session } from "next-auth";
+import { redirect, useSearchParams } from "next/navigation";
+import { login } from "./actions";
+
+export default function Login({
+  session,
+}: {
+  session: Session | null;
+  redirectTo?: string;
+}) {
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") ?? "/";
+
+  if (session) {
+    return redirect(redirectTo);
+  }
+
+  async function handleLogin() {
+    await login(redirectTo);
+    redirect(redirectTo);
+  }
+
+  return (
+    <div className="container mx-auto mt-8 flex flex-col items-center gap-2 px-10">
+      <div className="card w-full max-w-80 bg-primary text-primary-content shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Iniciar sessió</h2>
+          <p>Has d&apos;iniciar sessió per a accedir a l&apos;aplicació</p>
+          <div className="card-actions mt-4">
+            <form onSubmit={handleLogin} className="w-full">
+              <button className="btn w-full">Entrar amb Google</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
