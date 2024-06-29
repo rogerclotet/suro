47218b5450217dd753873627957c8b87762b6d1b
Roger Clotet <roger@clotet.dev>
2024-06-29 23:15:33 +0200

    Improve empty state style

--- src/app/projectes/[projectId]/llistes/_components/lists.tsx
+++ src/app/projectes/[projectId]/llistes/_components/lists.tsx
@@ -10,7 +10,7 @@ export default async function Lists({ projectId }: { projectId: string }) {
     return (
       <div className="flex flex-row items-center justify-end gap-4 pr-8 text-right md:pr-14">
         Encara no hi ha llistes, pots crear-ne una aquí{" "}
-        <CornerRightUp className="sm:mb-4" />
+        <CornerRightUp className="mb-4 flex-shrink-0" />
       </div>
     );
   }
