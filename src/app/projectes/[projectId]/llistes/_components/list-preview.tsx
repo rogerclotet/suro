4a94a63a675993f3b2a8bce1e0020d131225e69b
Roger Clotet <roger@clotet.dev>
2024-06-29 23:00:32 +0200

    Add todo count to list preview

--- src/app/projectes/[projectId]/llistes/_components/list-preview.tsx
+++ src/app/projectes/[projectId]/llistes/_components/list-preview.tsx
@@ -2,13 +2,19 @@ import type { List } from "@/app/_data/list";
 import Link from "next/link";
 
 export default function ListPreview({ list }: { list: List }) {
+  const todoCount = list.items.filter((item) => item.completed).length;
+
   return (
     <Link href={`/projectes/${list.projectId}/llistes/${list.id}`}>
-      <div className="card bg-base-200 shadow-xl">
+      <div className="card bg-base-300 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title">{list.name}</h2>
+          <div className="flex items-center justify-between gap-2">
+            <h2 className="card-title">{list.name}</h2>
+            {todoCount > 0 && (
+              <span className="badge badge-lg">{todoCount}</span>
+            )}
+          </div>
           <p className="line-clamp-2">{list.description}</p>
-          <p>{list.items.length} elements</p>
         </div>
       </div>
     </Link>
