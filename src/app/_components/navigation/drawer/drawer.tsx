287c5fc3a1c46ec2875acf1c33761afbc848ebc8
Roger Clotet <roger@clotet.dev>
2024-07-01 16:06:46 +0200

    Remove assert from drawer

--- src/app/_components/navigation/drawer/drawer.tsx
+++ src/app/_components/navigation/drawer/drawer.tsx
@@ -1,12 +1,13 @@
 import { auth } from "@/auth";
 import { getProjects } from "@/server/projects";
-import assert from "assert";
 import DrawerLayout from "./drawer-layout";
 import Profile from "./profile";
 
 export default async function Drawer() {
   const session = await auth();
-  assert(session, "Unauthenticated user");
+  if (!session) {
+    return null;
+  }
 
   const projects = await getProjects();
 
