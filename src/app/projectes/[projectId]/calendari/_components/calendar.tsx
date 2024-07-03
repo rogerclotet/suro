809b2c4f726c8edc0ae0194e0bf2214a1d3c65c4
Roger Clotet <roger@clotet.dev>
2024-07-04 00:11:43 +0200

    Use react-calendar

--- src/app/projectes/[projectId]/calendari/_components/calendar.tsx
+++ src/app/projectes/[projectId]/calendari/_components/calendar.tsx
@@ -1,89 +1,41 @@
 "use client";
 
-import "@/styles/calendar.css";
-import { ca } from "date-fns/locale";
 import React from "react";
-import RLCalendar from "react-lightweight-calendar";
-import { CurrentView } from "react-lightweight-calendar/dist/components/Calendar/Calendar.types";
+import ReactCalendar from "react-calendar";
+import "src/styles/calendar.css";
 
-type Event = {
-  id: string;
-  startTime: string;
-  endTime: string;
-  title: string;
-  bgColor?: string;
-  textColor?: string;
-};
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function Calendar() {
-  const [currentDate, setCurrentDate] = React.useState<string>(
-    new Date().toISOString().split("T")[0]!,
-  );
-  const [events, setEvents] = React.useState<Event[]>([]);
-  const [view, setView] = React.useState<CurrentView>(CurrentView.WEEK_TIME);
-
-  function addEvent(event: Event) {
-    setEvents((prevEvents) => [...prevEvents, event]);
-  }
+  const [value, setValue] = React.useState<Value>(new Date());
 
   return (
-    <div className="calendar">
+    <div className="calendar mb-8">
       <div className="mb-4 flex items-center justify-between gap-4">
         <h1 className="text-xl font-semibold">Calendari</h1>
-        <div className="flex items-center gap-2">
-          Vista:
-          <select
-            value={view}
-            onChange={(e) => setView(e.target.value as CurrentView)}
-            className="select"
-          >
-            <option value={CurrentView.WEEK_TIME}>Setmana</option>
-            <option value={CurrentView.MONTH}>Mes</option>
-          </select>
-        </div>
       </div>
 
-      <RLCalendar
-        data={[...events]}
-        currentView={view}
-        currentDate={currentDate}
-        setCurrentDate={(date) => setCurrentDate(date as string)}
-        activeTimeDateField="startTime-endTime" // Or just startTime or just endTime
-        weekStartsOn={1} // Monday
-        timeDateFormat={{
-          day: "EE",
-          hour: "HH:mm",
-          monthYear: "LLLL yyyy",
-        }}
-        locale={ca}
-        renderItemText={(item) => (
-          <div className="flex flex-col gap-1">
-            <div className="text-sm font-semibold">{item.title}</div>
-            <div className="text-xs">
-              {new Date(item.startTime as string).toLocaleTimeString("ca-ES", {
-                hour: "numeric",
-                minute: "2-digit",
-              })}{" "}
-              -{" "}
-              {new Date(item.endTime as string).toLocaleTimeString("ca-ES", {
-                hour: "numeric",
-                minute: "2-digit",
-              })}
-            </div>
-          </div>
-        )}
-        onCellClick={(value) => {
-          const endTime = new Date(value.timeDateUTC);
-          endTime.setHours((value.hour ?? 0) + 1);
-
-          addEvent({
-            id: Math.random().toString(),
-            startTime: value.timeDateUTC,
-            endTime: endTime.toISOString(),
-            title: "Esdeveniment de prova",
-          });
-        }}
+      <ReactCalendar
+        value={value}
+        onChange={setValue}
+        locale="ca-ES"
+        className="mx-auto mb-8 max-w-[600px] rounded-xl bg-base-300 p-1 shadow-lg"
       />
+
+      {value && (
+        <div>
+          <h2 className="mb-4 text-xl font-semibold">
+            {value.toLocaleString("ca-ES", {
+              dateStyle: "long",
+            })}
+          </h2>
+
+          <p className="italic opacity-60">
+            No hi ha cap esdeveniment programat per aquest dia.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
