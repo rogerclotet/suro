7c9851ef232072fea5d136202e29e0c1030693a0
Roger Clotet <roger@clotet.dev>
2024-07-12 15:40:03 +0200

    Reduce space between incomplete and complete lists

--- src/app/projectes/[projectId]/llistes/_components/lists.tsx
+++ src/app/projectes/[projectId]/llistes/_components/lists.tsx
@@ -33,7 +33,7 @@ export default async function Lists({ projectId }: { projectId: string }) {
   completedLists.sort(compareLists);
 
   return (
-    <div className="space-y-12">
+    <div className="space-y-6">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {incompleteLists.map((list) => (
           <ListPreview key={list.id} list={list} />
