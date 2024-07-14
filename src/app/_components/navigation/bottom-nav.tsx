164ea25454a32bb59bfd2aa7c67a1be0e3ce93c9
Roger Clotet <roger@clotet.dev>
2024-07-15 00:06:14 +0200

    Change bottom nav disabled items

--- src/app/_components/navigation/bottom-nav.tsx
+++ src/app/_components/navigation/bottom-nav.tsx
@@ -15,20 +15,29 @@ export default function BottomNav() {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-40 flex flex-row bg-background text-xs lg:hidden">
-      {menuItems.map((item) => (
-        <Link
-          key={item.name}
-          href={item.path}
-          className={cn(
-            "flex flex-grow flex-col items-center justify-center gap-2 p-2 text-primary",
-            isActive(item.path) && "border-t-2 border-primary",
-            item.disabled && "bg-muted text-muted-foreground",
-          )}
-        >
-          {item.name}
-          {item.icon}
-        </Link>
-      ))}
+      {menuItems.map((item) =>
+        item.disabled ? (
+          <div
+            key={item.name}
+            className="flex flex-grow flex-col items-center justify-center gap-2 bg-muted p-2 text-muted-foreground"
+          >
+            {item.name}
+            {item.icon}
+          </div>
+        ) : (
+          <Link
+            key={item.name}
+            href={item.path}
+            className={cn(
+              "flex flex-grow flex-col items-center justify-center gap-2 p-2 text-primary",
+              isActive(item.path) && "border-t-2 border-primary",
+            )}
+          >
+            {item.name}
+            {item.icon}
+          </Link>
+        ),
+      )}
     </div>
   );
 }
