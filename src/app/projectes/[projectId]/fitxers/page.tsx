32eb12c0bbe4948ac4660a377aa47c22aba24a37
Roger Clotet <roger@clotet.dev>
2024-07-16 19:11:06 +0200

    Empty state for files page

--- src/app/projectes/[projectId]/fitxers/page.tsx
+++ src/app/projectes/[projectId]/fitxers/page.tsx
@@ -1,4 +1,5 @@
 import getFiles from "@/server/files";
+import { CornerRightUp } from "lucide-react";
 import FilePreview from "./_components/file-preview";
 import UploadButton from "./_components/upload-button";
 
@@ -16,11 +17,18 @@ export default async function FilesPage({
         <UploadButton projectId={projectId} />
       </div>
 
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {allFiles.map((file) => (
-          <FilePreview key={file.id} file={file} />
-        ))}
-      </div>
+      {allFiles.length === 0 ? (
+        <div className="flex flex-row items-center justify-end gap-4 pr-8 text-right sm:pr-16">
+          Encara no hi ha fitxers, en pots compartir aquí{" "}
+          <CornerRightUp className="mb-4 flex-shrink-0" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {allFiles.map((file) => (
+            <FilePreview key={file.id} file={file} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
