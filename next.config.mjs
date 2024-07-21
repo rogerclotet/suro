73bd59cd8b4a0d0b4d222a59ea7a59f33c9122a5
Roger Clotet <roger@clotet.dev>
2024-07-21 17:52:32 +0200

    Add rewrite for posthog

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,19 @@ const coreConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 import { withSentryConfig } from "@sentry/nextjs";
