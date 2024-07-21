cafaa0d84d90d4a47d5228503d7d2cff03e28d4d
Roger Clotet <roger@clotet.dev>
2024-07-21 15:13:24 +0200

    Fix redirect

--- src/app/_components/redirect.tsx
+++ src/app/_components/redirect.tsx
@@ -2,24 +2,27 @@
 
 import { Loader2 } from "lucide-react";
 import { redirect } from "next/navigation";
+import React from "react";
 import type { Project } from "../_data/project";
 import { useProjects } from "../_state/project-state";
 
 export default function Redirect({ project }: { project?: Project }) {
   const { projects, project: selectedProject, selectProject } = useProjects();
 
-  if (project) {
-    selectProject(project);
-  } else if (!selectedProject) {
-    if (projects.length === 0) {
-      return (
-        <div className="flex h-[400px] items-center justify-center">
-          <Loader2 className="h-12 w-12 animate-spin" />
-        </div>
-      );
+  React.useEffect(() => {
+    if (project) {
+      selectProject(project);
+    } else if (!selectedProject) {
+      selectProject(projects[0]!);
     }
+  }, [project, projects, selectedProject, selectProject]);
 
-    selectProject(projects[0]!);
+  if (projects.length === 0) {
+    return (
+      <div className="flex h-[400px] items-center justify-center">
+        <Loader2 className="h-12 w-12 animate-spin" />
+      </div>
+    );
   }
 
   return redirect(`/projectes/${project?.id ?? selectedProject?.id}/llistes`);
