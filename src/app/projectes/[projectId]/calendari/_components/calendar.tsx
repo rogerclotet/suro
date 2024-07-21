dc0fb1e42b9b039e202eaeee0a551c78ebe4ff75
Roger Clotet <roger@clotet.dev>
2024-07-21 11:46:26 +0200

    Replace url in calendar

--- src/app/projectes/[projectId]/calendari/_components/calendar.tsx
+++ src/app/projectes/[projectId]/calendari/_components/calendar.tsx
@@ -110,7 +110,7 @@ export default function Calendar() {
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
       const dateString = `${year}-${month}-${day}`;
-      router.push(`/projectes/${project?.id}/calendari?d=${dateString}`);
+      router.replace(`/projectes/${project?.id}/calendari?d=${dateString}`);
     }
   }
 
