64af2d6dca64bceeebdef621020fb558577233b9
Roger Clotet <roger@clotet.dev>
2024-07-28 23:59:47 +0200

    Fix variable name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 (
   sshpass -p $SSH_PASSWORD ssh $SSH_USERNAME@$SSH_IP -o StrictHostKeyChecking=no <<-EOF
     source ~/.bashrc
-    cd $SSH_PROJECT_FOLDER
+    cd $SSH_PROJECT_DIRECTORY
     git pull
     docker build -t familia .
     docker run -p $PORT:3000 familia
