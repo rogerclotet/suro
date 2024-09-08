e895a9d8e1f2f3171e476ad38de7589d19ee3606
Roger Clotet <roger@clotet.dev>
2024-09-08 12:14:26 +0200

    Build and push image to gitlab registry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,9 @@
 (
   sshpass -p $SSH_PASSWORD ssh $SSH_USERNAME@$SSH_IP -o StrictHostKeyChecking=no <<-EOF
     source ~/.bashrc
-    cd $SSH_PROJECT_DIRECTORY
-    git pull
-    docker build -t familia .
+    docker pull $IMAGE_TAG
     docker stop familia
-    docker rm familia
-    docker run --name=familia --restart=unless-stopped -d -p $PORT:3000 familia
+    docker run --name=familia --restart=unless-stopped -d -p $PORT:3000 $IMAGE_TAG
+    docker image rm -f $(docker images -a | grep -v "$IMAGE_TAG" | awk 'NR>1 {print $3}')
 EOF
 )
