0d3cb99f6c3be9aa3e9d65d305944ef2d8d51303
Roger Clotet <roger@clotet.dev>
2024-10-29 20:30:30 +0100

    Add notification click listener

--- public/sw.js
+++ public/sw.js
@@ -24,3 +24,22 @@ self.addEventListener("push", function (event) {
       }),
   );
 });
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === "/" && "focus" in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow("/");
+      }),
+  );
+});
