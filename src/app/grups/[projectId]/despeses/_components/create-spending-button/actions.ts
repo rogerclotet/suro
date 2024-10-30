387a3347726a15326d31b039be774afe75ee26c1
Roger Clotet <roger@clotet.dev>
2024-10-30 22:27:41 +0100

    Send notification on new spending

--- src/app/grups/[projectId]/despeses/_components/create-spending-button/actions.ts
+++ src/app/grups/[projectId]/despeses/_components/create-spending-button/actions.ts
@@ -7,6 +7,8 @@ import { Logger } from "next-axiom";
 import { revalidatePath } from "next/cache";
 import * as v from "valibot";
 import { spendingSchema } from "./data";
+import { sendPushNotification } from "@/server/push";
+import { getUserProject } from "@/server/projects";
 
 export async function createSpending(
   projectId: string,
@@ -15,6 +17,11 @@ export async function createSpending(
   const session = await auth();
   if (!session) {
     throw new Error("Unauthorized");
+  }
+
+  const project = await getUserProject(projectId);
+  if (project?.users.find((u) => u.user.id === session.user.id) === undefined) {
+    throw new Error("The user is not part of the project");
   }
 
   const parsedData = v.parse(spendingSchema, data);
@@ -40,6 +47,22 @@ export async function createSpending(
     });
     throw new Error("Error creating spending", { cause: e });
   }
+
+  setTimeout(() => {
+    sendPushNotification(
+      project,
+      parsedData.description
+        ? `Nova despesa: ${parsedData.description} (${parsedData.amount}€)`
+        : `Nova despesa: ${parsedData.amount}€`,
+      project.name,
+      `/grups/${project.id}/despeses`,
+    ).catch((err) => {
+      console.error(
+        "Failed to send push notification after creating spending",
+        err,
+      );
+    });
+  }, 0);
 
   revalidatePath(`/grups/${projectId}/despeses`);
 }
