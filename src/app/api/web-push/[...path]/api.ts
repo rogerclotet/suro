a0fe99a85523607b8449f7b454ea66c1664d91cd
Roger Clotet <roger@clotet.dev>
2024-10-30 16:06:23 +0100

    Remove send push endpoint

--- src/app/api/web-push/[...path]/api.ts
+++ src/app/api/web-push/[...path]/api.ts
@@ -1,7 +1,3 @@
-type NotificationOptions = Parameters<
-  ServiceWorkerRegistration["showNotification"]
->[1] & { title?: string };
-
 export async function submitSubscription(subscription: PushSubscription) {
   await fetch("/api/web-push/subscription", {
     method: "POST",
@@ -9,25 +5,5 @@ export async function submitSubscription(subscription: PushSubscription) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ subscription }),
-  });
-}
-
-export async function sendPush(
-  message: string,
-  title?: string,
-  path?: string,
-): Promise<void> {
-  const payload: NotificationOptions = {
-    title,
-    body: message,
-    data: { path: path ?? "/" },
-  };
-
-  await fetch("/api/web-push/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
   });
 }
--- src/app/api/web-push/[...path]/route.ts
+++ src/app/api/web-push/[...path]/route.ts
@@ -1,8 +1,6 @@
-import type { Project } from "@/app/_data/project";
 import { auth } from "@/auth";
 import { db } from "@/server/db";
 import { pushSubscriptions } from "@/server/db/schema";
-import { sendPushNotification } from "@/server/push";
 import { type PushSubscription } from "web-push";
 
 export async function POST(request: Request) {
@@ -10,8 +8,6 @@ export async function POST(request: Request) {
   switch (pathname) {
     case "/api/web-push/subscription":
       return setSubscription(request);
-    case "/api/web-push/send":
-      return sendPush(request);
     default:
       return new Response("Not found", { status: 404 });
   }
@@ -33,33 +29,6 @@ async function setSubscription(request: Request) {
   await db
     .insert(pushSubscriptions)
     .values({ subscription: body.subscription, userId: session.user.id });
-
-  return new Response();
-}
-
-async function sendPush(project: Project, request: Request) {
-  const session = await auth();
-  if (!session) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-
-  const payload = (await request.json()) as {
-    message: string;
-    title?: string;
-    path?: string;
-  };
-
-  try {
-    await sendPushNotification(
-      project,
-      payload.message,
-      payload.title,
-      payload.path,
-    );
-  } catch (error) {
-    console.error("Failed to send push notification", error);
-    return new Response("Failed to send push notification", { status: 500 });
-  }
 
   return new Response();
 }
