42f817290b54c8447e2cf717d55a9b960d829cae
Roger Clotet <roger@clotet.dev>
2025-10-30 04:34:04 +0100

    Fix format and imports

--- .lintstagedrc.ts
+++ .lintstagedrc.ts
@@ -4,7 +4,7 @@ import type { Configuration } from "lint-staged";
 
 export default {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "vitest related run"],
-  "*.{js,jsx,mjs,ts,tsx,json,html,css}": ["biome check --write"],
+  "*.{js,jsx,mjs,ts,tsx,json,html,css}": [() => "biome check --write"],
   "*.{ts,tsx}": [() => "tsc -p tsconfig.json --noEmit"],
   ".biome.json": ["biome migrate --write"],
 } satisfies Configuration;
