aa5d40ef6a9fb32f98b9b7d6649a3826ef4ec700
Roger Clotet <roger@clotet.dev>
2025-11-10 00:33:40 +0100

    Enable react query devtools based on localStorage variable

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { type PropsWithChildren, useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { type PropsWithChildren, useEffect, useState } from "react";
 
 function ReactQueryProvider({ children }: PropsWithChildren) {
   const [client] = useState(new QueryClient());
+  const [enableDevTools, setEnableDevTools] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setEnableDevTools(localStorage.getItem("query-devtools") === "true");
+  }, []);
 
   return (
     <QueryClientProvider client={client}>
       {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+
+      {enableDevTools && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 }
