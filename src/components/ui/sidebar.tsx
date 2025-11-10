d30f8c888ba7688b6668e83a7642c435075921af
Roger Clotet <roger@clotet.dev>
2025-11-10 21:47:48 +0100

    Set cursor pointer to sidebar menu button

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -517,7 +517,11 @@ function SidebarMenuButton({
       data-sidebar="menu-button"
       data-size={size}
       data-active={isActive}
-      className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+      className={cn(
+        "cursor-pointer",
+        sidebarMenuButtonVariants({ variant, size }),
+        className,
+      )}
       {...props}
     />
   );
