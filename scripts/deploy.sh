b69259b123893b596a5d364c7c30e98258010727
Roger Clotet <roger@clotet.dev>
2026-01-27 17:01:21 +0000

    Ignore errors stopping or removing container

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@
     cd $SSH_PROJECT_DIRECTORY
     git pull
     docker build -t familia .
-    docker stop familia
-    docker rm familia
+    docker stop familia 2>/dev/null || true
+    docker rm familia 2>/dev/null || true
     docker run --name=familia --restart=unless-stopped -d -p $PORT:3000 familia
 EOF
 )
